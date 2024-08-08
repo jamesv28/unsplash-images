ecffdb027402276f9d5b33575656dcb697c1669f
James Volmert <james.volmert@gmail.com>
2024-08-08 17:08:18 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import ThemToggle from "./components/ThemToggle";
+import Gallery from "./components/Gallery";
+import SearchForm from "./components/SearchForm";
+
+const App = () => {
+  return (
+    <main>
+      <ThemToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;

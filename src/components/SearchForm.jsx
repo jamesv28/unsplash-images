44a3872ebdae8f65155086f69e9ab281c40ca1b1
James Volmert <james.volmert@gmail.com>
2024-08-12 17:08:34 -0600

    added react query

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { useState } from "react";
 
 const SearchForm = () => {
-  return <div></div>;
+  const [search, setSearch] = useState("");
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (!e.target.elements.search.value) return;
+    console.log("search", e.target.elements.search.value);
+  };
+  return (
+    <section>
+      <h1 className="title">Upsplash Images</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          className="form-input "
+          name="search"
+          placeholder="Search images"
+        />
+        <button type="submit" className="btn">
+          Search
+        </button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { AppProvider } from "./context";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import App from "./App";
 import "./index.css";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <AppProvider>
-    <App />
+    <QueryClientProvider client={queryClient}>
+       
+      <App />
+    </QueryClientProvider>
   </AppProvider>
 );
